4896a4a49e4f9eb47053294c8b2fe66a381cbb39
kartik sedha <70266078+kartik5151@users.noreply.github.com>
2022-03-30 22:06:09 +0530

    Add files via upload
    In this program we will add two variable with user input, which means user will input the two numbers .

---
+++ Add two number with user input/main.py
@@ -0,0 +1,13 @@
+# This program will add two numbers with user input
+
+x = input('Enter First Number')
+y = input('Enter Second Number')
+
+# int(x) and int(y) defines that input number should be integer
+sum = int(x) + int(y)
+# we can write  data types according to our needs
+
+print("The sum is", sum)
+
+
+# Same program can be use for different data types - float and complex
