405e47c162eb1cb2d11d77a8320e205f43fbcc8a
kartik sedha <70266078+kartik5151@users.noreply.github.com>
2022-03-31 11:36:49 +0530

    Add files via upload
    In this program we will find square root of a number without user input

---
+++ Find square root/sqrt.py
@@ -0,0 +1,13 @@
+#program for finding square root
+
+#to solve math problems we use 'import math'- for posite number and 'import cmath'- for complex number
+
+
+import math #for positive numbers
+x=10
+print(math.sqrt(x))
+
+
+import cmath #for complex numbers
+y=10j+7
+print(cmath.sqrt(y))
